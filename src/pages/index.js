2a5dab7c11f5714841c142d3e46db1866e3088e0
Cleiton Alves <cleiton_jc@yahoo.com.br>
2023-09-04 14:03:47 -0300

    list covid 19 cases

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react";
 import { Title } from "../components/Title";
 
 export default function Home() {
+  const [list, setList] = useState([])
+
+  useEffect(() => {
+    (async () => {
+      const listStates = await (await fetch('https://covid19-brazil-api.vercel.app/api/report/v1')).json()
+
+      setList(listStates.data)
+    })()
+  }, [])
+
   return (
-    <Title>Home</Title>
+    <>
+      <Title>Home</Title>
+
+      <ul>
+        {list.map(item => (
+          <li key={item.uid}>
+            {item.state} - {item.cases.toLocaleString('pt-br')}
+          </li>
+        ))}
+      </ul>
+    </>
   )
 }
